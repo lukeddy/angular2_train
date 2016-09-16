6b3f62c0f8a0b2827643e7ecf9a8f50e007c2b11
tzq668766 <tzq618766@gmail.com>
2016-09-16 19:43:43 +0800

    angular2学习-你好Angular2

---
+++ helloworld/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'my-app',
+    template:'<h5 class="well"><i class="glyphicon glyphicon-thumbs-up"></i> 你好，Angular2,我是来自angular2的内容</h5>'
+})
+
+export class AppComponent{}
