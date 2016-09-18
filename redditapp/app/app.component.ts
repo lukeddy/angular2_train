1922893facc4b38805612e169b2e688110ce232a
yocool <544301172@qq.com>
2016-09-18 17:22:38 +0800

    reddit app示例，项目初始化

---
+++ redditapp/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'my-app',
+    // template:'<h5 class="well"><i class="glyphicon glyphicon-thumbs-up"></i> 你好，Angular2,我是来自angular2的内容</h5>'
+    templateUrl:'./template/myapp-template.html'
+})
+
+export class AppComponent{}
